7d95ee2e4e36dfbe2cb53d19a65933b54796ec7d
su-yash-7 <suyashpradhan7@gmail.com>
2021-06-03 17:55:26 +0530

    About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import home1 from '../img/home1.png';
 import {About,Description,Image,Hide} from '../styles';
 import {motion} from 'framer-motion';
-
+import {titleAnim,fade,photoAnim} from '../animation';
 const AboutSection = () =>{
     
     return(
@@ -10,20 +10,20 @@ const AboutSection = () =>{
              <Description>
                 <motion.div className="title">
                     <Hide>
-                        <motion.h2 >We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography videography ideas that you have. We have professionals with amezing skils.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any photography videography ideas that you have. We have professionals with amezing skils.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera"/>
+                <motion.img variants={photoAnim}src={home1} alt="guy with a camera"/>
             </Image>
         </About>
     )
